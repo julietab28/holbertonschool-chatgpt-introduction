63631969fe504a79cd33a5c8843e628b3921be62
Julieta Bobadilla <julietabobadilla123@gmail.com>
2024-09-03 18:23:10 +0000

    1 - Fixed again the code so it prints the factorial of an argument

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -6,20 +6,8 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1  # Decrementa n en cada iteración
+	n = n - 1
     return result
 
-if len(sys.argv) != 2:
-    print("Uso: {} <número>".format(sys.argv[0]))
-    sys.exit(1)
-
-try:
-    number = int(sys.argv[1])
-    if number < 0:
-        raise ValueError("El número debe ser no negativo.")
-except ValueError as e:
-    print(f"Error: {e}")
-    sys.exit(1)
-
-f = factorial(number)
+f = factorial(int(sys.argv[1]))
 print(f)
